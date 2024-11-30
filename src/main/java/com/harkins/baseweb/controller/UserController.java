95a4f224f5113f13b6c6d99575abef8f918e1cea
Harkins <tmngoc1108@gmail.com>
2024-11-30 21:39:23 +0700

    add tk dc roi

--- src/main/java/com/harkins/baseweb/controller/UserController.java
+++ src/main/java/com/harkins/baseweb/controller/UserController.java
@@ -1,8 +1,22 @@
 package com.harkins.baseweb.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.harkins.baseweb.model.Users;
+import com.harkins.baseweb.service.UserService;
+
 
 @RestController
 public class UserController {
+    @Autowired
+    private UserService service;
+
+    @PostMapping("/register")
+    public Users register(@RequestBody Users user) {
+        return service.register(user);
+    }
     
 }
---
+++ src/main/java/com/harkins/baseweb/service/UserService.java
@@ -0,0 +1,21 @@
+package com.harkins.baseweb.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.harkins.baseweb.model.Users;
+import com.harkins.baseweb.repo.UserRepo;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepo repo;
+
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
+    public Users register(Users user) {
+        // user.setPassword(encoder.encode(user.getPassword()));
+        return repo.save(user);
+    }
+}
