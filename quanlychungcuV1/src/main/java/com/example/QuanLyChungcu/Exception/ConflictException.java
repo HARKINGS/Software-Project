12811db2eeccca68ac329935ce71960b0310db45
helloimgnud <160420980+helloimgnud@users.noreply.github.com>
2024-12-12 05:52:58 +0700

    delete

--- quanlychungcuV1/src/main/java/com/example/QuanLyChungcu/Exception/ConflictException.java
+++
@@ -1,7 +0,0 @@
-package com.example.QuanLyChungcu.Exception;
-
-public class ConflictException extends RuntimeException{
-    public ConflictException(String message) {
-        super(message);
-    }
-}
--- quanlychungcuV1/src/main/java/com/example/QuanLyChungcu/Exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.QuanLyChungcu.Exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
